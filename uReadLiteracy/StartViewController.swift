e35cbd50e9175c91675d2ce1e8b8b0db845cbc66
Duy Le <leanhduy1998@gmail.com>
2018-11-01 23:41:39 -0400

    renamed viewcontroller to StartViewController, reorganized files into folders

--- uReadLiteracy/StartViewController.swift
+++ uReadLiteracy/StartViewController.swift
@@ -8,26 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class StartViewController: UIViewController {
+    
+    @IBOutlet weak var continueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showWalkthroughIfFirstTime()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
     override func viewDidAppear(_ animated: Bool) {
-      
-     
+        super.viewDidAppear(animated)
+        animateContinueBtn()
+    }
+    
+    private func animateContinueBtn(){
+        UIView.animate(withDuration: 0.7, delay: 0, options: [.autoreverse,.repeat], animations: {
+            self.continueLabel.alpha = 0.5
+        }, completion: nil)
+    }
+    
+    private func showWalkthroughIfFirstTime(){
         if UserDefaults.standard.bool(forKey: "hasViewedWalkthrough"){
             return
         }
- 
- 
+        
         let storyboard = UIStoryboard(name: "Tutorial", bundle: nil)
         if let walkthroughViewController = storyboard.instantiateViewController(withIdentifier: "WalkthroughViewController") as? WalkthroughViewController {
             present(walkthroughViewController, animated: true, completion: nil)
