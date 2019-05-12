1a3b8298d32b2a578aea3cc09562ec65f2919961
Duy Le <leanhduy1998@gmail.com>
2019-05-12 09:15:32 -0400

    change start screen and add launch screen

--- uReadLiteracy/StartViewController.swift
+++ uReadLiteracy/StartViewController.swift
@@ -15,32 +15,13 @@ import AVKit
 class StartViewController: UIViewController {
     
     
-    @IBOutlet weak var bookAnimationView: LOTAnimationView!
-    
-    @IBOutlet weak var continueLabel: UILabel! 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        bookAnimationView.loopAnimation = true
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        animateContinueBtn()
-        bookAnimationView.play()
-    }
-    
-    private func animateContinueBtn(){
-        UIView.animate(withDuration: 0.7, delay: 0, options: [.autoreverse,.repeat], animations: {
-            self.continueLabel.alpha = 0.5
-        }, completion: nil)
-    }
-    
-    @IBAction func continueBtnPressed(_ sender: Any) {
+        
         if UserDefaults.standard.bool(forKey: "hasViewedWalkthrough"){
             let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let vc = storyboard.instantiateViewController(withIdentifier: "MainTabBarViewController")
@@ -53,6 +34,5 @@ class StartViewController: UIViewController {
         let vc = storyboard.instantiateViewController(withIdentifier: "MainTabBarViewController")
         self.show(vc, sender: self)
     }
-
 }
 
